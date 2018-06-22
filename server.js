cef0fa27613b40337afe0c9b92df808a24e213c3
MaggieB29 <anaritamparo@hotmail.com>
2018-06-22 12:05:13 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const request = require('request');
+const yargs = require('yargs');
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+app.set('view engine', 'hbs');
+
+app.get('/', (request, response) => {
+    response.render('index.hbs', {
+        
+    });    
+});
+
+app.get('/weather', (req, res) => {
+    //res.send(req.query.cidade);
+    const GoogleAPI = 'AIzaSyDMgsp68WJ4MwNY9u66TY2nuDGiTtaS9gc';
+    const DarkSky = '85693fe78a243ea6c99ec85b0f8263ef';
+
+    var address = req.query.cidade; //a é o nome da cena, tem de ter sempre um nome
+//para verificar node app.js -a "*coisas*"
+//para chamar metemos o nome na console e a rua
+/*"D. Sancho I, Vila do Conde";*/
+var addressEncoded = encodeURIComponent(address);
+
+request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${addressEncoded}&key=${GoogleAPI}`,
+    json: true
+}, (error, response, body) => {
+    var lat = body.results[0].geometry.location.lat; //chama a latitude
+    var lng = body.results[0].geometry.location.lng;
+    var formatted_address = body.results[0].formatted_address;
+
+    console.log(formatted_address); //imprimi o adereço todo bonito
+
+    request({
+        url: `https://api.darksky.net/forecast/${DarkSky}/${lat},${lng}?units=si`,
+        json: true
+    }, (DSerror, Dresponse, DSbody)=> {
+
+        var temperature = DSbody.currently.temperature;
+        var apparentTemperature = DSbody.currently.apparentTemperature;
+
+        console.log(temperature, apparentTemperature);
+    })
+});
+});
+
+app.listen(3002);
+
+//ERROS
+
+/*app.use((req, res, next) => {
+    res.status(404).send("Desculpa! Não há ligação à internet!")
+  })
+  
+  app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+  })*/
